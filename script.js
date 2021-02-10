03aa812282f5ac6f67a3b275aa481e73c072fdfc
V1999-30 <berzosa.valentin@gmail.com>
2021-02-10 11:21:06 +0100

    Page d'accueil

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-/*connexion avec l'API*/
 getAllFurnitures = () => {
     return new Promise((resolve) => {
       let request = new XMLHttpRequest();
       request.onreadystatechange = function () {
         if (
           this.readyState == XMLHttpRequest.DONE &&
-          this.status >= 200) {
+          this.status >= 200 &&
+          this.status < 400
+        ) {
           resolve(JSON.parse(this.responseText));
           console.log("Connecté");
         } else {
         }
       };
-      request.open("GET", "http://localhost:3000/api/furniture");
+      request.open("GET", "http://localhost:3000/api/furniture/");
       request.send();
     });
   };
@@ -19,11 +20,12 @@ getAllFurnitures = () => {
   async function furnitures() {
     const furnitures = await getAllFurnitures();
   
+    /* Lien avec la page index HTML */
   
-    let products = document.getElementById("products");
-
-    /* création du HTML */
-    
+    let productsList = document.getElementById("products_list");
+  
+    /* création de la structure index HTML */
+  
     furnitures.forEach((meuble) => {
       let productContainer = document.createElement("section");
       let productImg = document.createElement("figure");
@@ -32,32 +34,32 @@ getAllFurnitures = () => {
       let productName = document.createElement("h3");
       let productPrice = document.createElement("p");
       let productLink = document.createElement("a");
-
-      /* Ajout des attributs - HTML */
+  
+      /*Ajout des attributs au balise index HTML */
       productContainer.setAttribute("class", "product_container mx-5 my-5");
       productImg.setAttribute("class", "product_img card shadow");
       productPhoto.setAttribute("src", meuble.imageUrl);
       productPhoto.setAttribute("class", "card-img-top");
       productPhoto.setAttribute("alt", "Image meuble en chêne");
       productElmnt.setAttribute("class", "product_elmnt card-body w-100 position-absolute");
-      productElmnt.setAttribute("style", "bottom: 0%;")
+      productElmnt.setAttribute("style", "bottom: 0%;");
       productName.setAttribute("class", "product_name card-title");
       productPrice.setAttribute("class", "product_price card-text");
-      productLink.setAttribute("href", "produit.html?id=" + meuble._id);
+      productLink.setAttribute("href", "test-produit.html?id=" + meuble._id);
       productLink.setAttribute("class", "btn btn-secondary");
-
-      /* Liste parents/enfants  - HTML */
-      products.appendChild(productContainer);
+  
+      /* Agencement des éléments index HTML */
+      productsList.appendChild(productContainer);
       productContainer.appendChild(productImg);
       productImg.appendChild(productPhoto);
       productImg.appendChild(productElmnt);
       productElmnt.appendChild(productName);
       productElmnt.appendChild(productPrice);
       productElmnt.appendChild(productLink);
-
-      /* Contenu bandeau-figcaption  - HTML */
+  
+      /* Contenu des balises index HTML */
       productName.textContent = meuble.name;
       productPrice.textContent = meuble.price / 100 + " euros";
-      productLink.textContent = "Voir le porduit";
-    })
+      productLink.textContent = "Voir le produit";
+    });
   }
